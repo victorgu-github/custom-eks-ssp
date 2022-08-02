d5f107ca6cb2eb0a82d69d7cdfc41ca8cf9c4d3f
EC2 Default User <ec2-user@ip-10-1-1-13.us-west-2.compute.internal>
2022-08-02 20:57:41 +0000

    add endpoint

--- fargate-serverless-VPC/main.tf
+++ fargate-serverless-VPC/main.tf
@@ -297,138 +297,6 @@ resource "null_resource" "remove_default_coredns_deployment" {
 # }
 
 
-resource "aws_elasticsearch_domain" "opensearch" {
-  domain_name           = "${local.name}-demo"
-  elasticsearch_version = "OpenSearch_1.2"
-
-  cluster_config {
-    instance_type          = "m6g.large.elasticsearch"
-    instance_count         = 1
-   
-  }
-
-  node_to_node_encryption {
-    enabled = true
-  }
-
-  domain_endpoint_options {
-    enforce_https       = true
-    tls_security_policy = "Policy-Min-TLS-1-2-2019-07"
-  }
-
-  encrypt_at_rest {
-    enabled = true
-  }
-  
-  ebs_options {
-    ebs_enabled = true
-    volume_size = 10
-  }
-
-  advanced_security_options {
-    enabled                        = true
-    internal_user_database_enabled = true
-
-    master_user_options {
-      master_user_name     = var.opensearch_dashboard_user
-      master_user_password = var.opensearch_dashboard_pw
-    }
-  }
-
-  tags = local.tags
-}
-
-
-
-// access policy in opensearch
-resource "aws_elasticsearch_domain_policy" "opensearch_access_policy" {
-  domain_name     = aws_elasticsearch_domain.opensearch.domain_name
-  access_policies = data.aws_iam_policy_document.opensearch_access_policy.json
-}
-data "aws_iam_policy_document" "opensearch_access_policy" {
-  statement {
-    effect    = "Allow"
-    resources = ["${aws_elasticsearch_domain.opensearch.arn}/*"]
-    actions   = ["es:ESHttp*"]
-    principals {
-      type        = "*"
-      identifiers = ["*"]
-    }
-  }
-}
-
-
-// access policy for fargate execution role 
-resource "aws_iam_policy" "fluentbit_opensearch_access" {
-  name        = "${local.name}-eks-fargate-logging-policy"
-  description = "IAM policy to allow Fluentbit access to OpenSearch"
-  policy      = data.aws_iam_policy_document.fluentbit_opensearch_access.json
-}
-data "aws_iam_policy_document" "fluentbit_opensearch_access" {
-  statement {
-    sid       = "OpenSearchAccess"
-    effect    = "Allow"
-    resources = ["${aws_elasticsearch_domain.opensearch.arn}/*"]
-    actions   = ["es:ESHttp*"]
-  }
-}
-
-//AMP
-module "managed_prometheus" {
-  source  = "terraform-aws-modules/managed-service-prometheus/aws"
-  version = "~> 2.1"
-
-  workspace_alias = local.name
-
-  tags = local.tags
-}
-
-# create irsa for adot 
-resource "aws_iam_role" "amp_ingest_role" {
-  name = "${local.name}-amp-ingest-irsa"
-
-  # Terraform's "jsonencode" function converts a
-  # Terraform expression result to valid JSON syntax.
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action = "sts:AssumeRoleWithWebIdentity"
-        Effect = "Allow"
-        Sid    = ""
-        Principal = {
-          Federated = "${module.eks_blueprints.eks_oidc_provider_arn}"
-        }
-        
-      },
-    ]
-  })
-  
-  inline_policy {
-    name = "my_inline_policy"
-
-    policy = jsonencode({
-      Version = "2012-10-17"
-      Statement = [
-        {
-          Action   = [
-             "aps:RemoteWrite", 
-             "aps:GetSeries", 
-             "aps:GetLabels",
-             "aps:GetMetricMetadata"
-          ]
-          Effect   = "Allow"
-          Resource = "*"
-        },
-      ]
-    })
-  }
-
-  tags = local.tags
-}
-
-
-
 
 #---------------------------------------------------------------
 # Supporting Resources
@@ -468,4 +336,84 @@ module "vpc" {
   }
 
   tags = local.tags
+}
+
+module "vpc_endpoint_gateway" {
+  source  = "terraform-aws-modules/vpc/aws//modules/vpc-endpoints"
+  version = "v3.2.0"
+
+  create = true
+  vpc_id = module.vpc.vpc_id
+
+  endpoints = {
+    s3 = {
+      service      = "s3"
+      service_type = "Gateway"
+      route_table_ids = flatten([
+        module.vpc.intra_route_table_ids,
+      module.vpc.private_route_table_ids])
+      tags = { Name = "s3-vpc-Gateway" }
+    },
+  }
+}
+
+data "aws_security_group" "default" {
+  name   = "default"
+  vpc_id = module.vpc.vpc_id
+}
+
+module "vpc_endpoints" {
+  source             = "terraform-aws-modules/vpc/aws//modules/vpc-endpoints"
+  version            = "v3.2.0"
+  create             = true
+  vpc_id             = module.vpc.vpc_id
+  security_group_ids = [data.aws_security_group.default.id]
+  subnet_ids         = module.vpc.private_subnets
+
+  endpoints = {
+    ssm = {
+      service             = "ssm"
+      private_dns_enabled = true
+    },
+    logs = {
+      service             = "logs"
+      private_dns_enabled = true
+    },
+    autoscaling = {
+      service             = "autoscaling"
+      private_dns_enabled = true
+    },
+    sts = {
+      service             = "sts"
+      private_dns_enabled = true
+    },
+    elasticloadbalancing = {
+      service             = "elasticloadbalancing"
+      private_dns_enabled = true
+    },
+    ec2 = {
+      service             = "ec2"
+      private_dns_enabled = true
+    },
+    ec2messages = {
+      service             = "ec2messages"
+      private_dns_enabled = true
+    },
+    ecr_api = {
+      service             = "ecr.api"
+      private_dns_enabled = true
+    },
+    ecr_dkr = {
+      service             = "ecr.dkr"
+      private_dns_enabled = true
+    },
+    kms = {
+      service             = "kms"
+      private_dns_enabled = true
+    }
+  }
+  tags = {
+    Project  = "EKS"
+    Endpoint = "true"
+  }
 }
